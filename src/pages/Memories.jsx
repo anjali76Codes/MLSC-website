c20ba83889eda7861402a85427097a2fee8c6fb5
Anjali Gupta <152984851+anjali76Codes@users.noreply.github.com>
2024-08-02 15:00:48 +0530

    Memories content done

--- src/pages/Memories.jsx
+++ src/pages/Memories.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import styles from "./Memories.module.css";
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
@@ -8,10 +8,22 @@ import video5 from "../assets/video5.mp4";
 import video6 from "../assets/video6.mp4";
 import video7 from "../assets/video7.mp4";
 import video8 from "../assets/video8.mp4";
+import video9 from "../assets/video7.mp4";
 
 const Memories = () => {
+  const [playingVideo, setPlayingVideo] = useState(null);
+  const videoRefs = useRef([]);
+
+  const handlePlay = (index) => {
+    if (playingVideo !== null && playingVideo !== index) {
+      videoRefs.current[playingVideo].pause();
+    }
+    setPlayingVideo(index);
+    videoRefs.current[index].play();
+  };
+
   const posts = [
-    { id: 1, videoUrl: video1, description: "Post 1 Description" },
+    { id: 1, videoUrl: video1, description: "MLSC conducting the coherence 3.0 the anjali" },
     { id: 2, videoUrl: video2, description: "Post 2 Description" },
     { id: 3, videoUrl: video3, description: "Post 3 Description" },
     { id: 4, videoUrl: video4, description: "Post 4 Description" },
@@ -19,6 +31,7 @@ const Memories = () => {
     { id: 6, videoUrl: video6, description: "Post 6 Description" },
     { id: 7, videoUrl: video7, description: "Post 7 Description" },
     { id: 8, videoUrl: video8, description: "Post 8 Description" },
+    { id: 9, videoUrl: video7, description: "Post 9 Description" },
     // Add more posts as needed
   ];
 
@@ -26,10 +39,16 @@ const Memories = () => {
     <div className={styles.container}>
       <h1 className={styles.heading}>Memories</h1>
       <div className={styles.grid}>
-        {posts.map(post => (
+        {posts.map((post, index) => (
           <div key={post.id} className={styles.card}>
-            <div className={styles.phoneBorder}>
-              <video controls muted className={styles.video}>
+            <div className={styles.videoBox} onClick={() => handlePlay(index)}>
+              <video
+                controls
+                controlsList="nodownload nofullscreen noremoteplayback"
+                muted
+                className={styles.video}
+                ref={(el) => (videoRefs.current[index] = el)}
+              >
                 <source src={post.videoUrl} type="video/mp4" />
                 Your browser does not support the video tag.
               </video>
